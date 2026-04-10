dffe2360f2e87b9fcde1fcedd5d5bfd8447a0681
miguelzack <mzsilvaw@gmail.com>
2026-04-10 10:44:20 -0300

    installing spring security in ecommerce

--- security/src/main/java/com/example/security/services/CustomUserDetailsService.java
+++ security/src/main/java/com/example/security/services/CustomUserDetailsService.java
@@ -21,10 +21,6 @@ public class CustomUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         Usuario usuario = usuarioRepository.findByEmail(email);
-        return User.builder()
-                .username(usuario.getEmail())
-                .password(usuario.getSenha())
-                .roles(usuario.getRole().name().replace("ROLE_", ""))
-                .build();
+        return User.builder().username(usuario.getEmail()).password(usuario.getSenha()).roles(usuario.getRole().name().replace("ROLE_", "")).build();
     }
 }
